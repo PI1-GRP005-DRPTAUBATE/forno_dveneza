38bff8dc8d180f258c1296e91c97d59842e70827
Ronald Barbosa <ronaldverissimo@hotmail.com>
2023-03-25 15:51:38 -0300

    Organizando estrutura dos apps

--- forno_dveneza/produtos/views.py
+++ forno_dveneza/produtos/views.py
@@ -2,18 +2,8 @@ from django.shortcuts import render
 
 from .models import Produto, Categoria
 
-def todos_produtos(request):
-    categorias = Categoria.objects.all()
-    teste1 = []
-    teste2 = []
-    for categoria in categorias:
-        teste1.append(categoria.pk)
-        teste2.append(categoria.descricao)
-        print(dir(categoria.descricao))
-        print(categoria.descricao.__str__())
-
-    
+def todos_produtos(request):    
     return render(request, 'produtos/todos.html', {
-        'categorias': categorias,
+        'categorias': Categoria.objects.all(),
         'produtos': Produto.objects.all()
     })
