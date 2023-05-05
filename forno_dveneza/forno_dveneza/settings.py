e8e9348f42955e977138cbd7eaa00ddd40900236
Francisco Reis Ramos <francisco.reis.ramos96@gmail.com>
2023-05-05 02:55:42 -0300

    Imagens de produtos
    Adição da possibilidade de inserir imagens dos produtos via admin.

--- forno_dveneza/forno_dveneza/settings.py
+++ forno_dveneza/forno_dveneza/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+	'cloudinary',
     'base',
     'clientes',
     'funcionarios',
