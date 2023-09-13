3eff004e368626c5a96839233cf3378968e8cd86
Rhulin <rhulioenrico2018@gmail.com>
2023-09-12 21:41:10 -0300

    Implementação da categoria borda

--- forno_dveneza/backend/produtos/models.py
+++ forno_dveneza/backend/produtos/models.py
@@ -35,4 +35,10 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.descricao
-    
+class ProdutoBorda(models.Model):
+    produto = models.CharField(max_length=100)
+    descricao = models.TextField()
+    preco_extra = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.produto
